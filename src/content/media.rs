813e4a27ca0ffe9e7063b78042f10fb7952c5589
kartikchechani <chechanikartik@gmail.com>
2025-09-01 23:37:39 +0530

    final project media

---
+++ src/content/media.rs
@@ -0,0 +1,33 @@
+#[derive(Debug)]
+pub enum Media {
+    Book {  title: String, author: String },
+    Movie { title: String, director: String },
+    Audiobook { title: String },
+    Podcast ( u32 ),
+    Placeholder
+
+
+}
+
+
+impl Media {
+    pub fn description(&self) -> String {
+        match self {
+            Media::Book {title, author} => {
+                format!("Book - Title: {}, Author: {}", title, author)
+            },
+            Media::Movie {title, director} => {
+                format!("Movie - Title: {}, Director: {}", title, director)
+            },
+            Media::Audiobook {title} => {
+                format!("Audiobook - Title: {}", title)
+            },
+            Media::Podcast(episode) => {
+                format!("Podcast - Episode: {}", episode)
+            },
+            Media::Placeholder => {
+                String::from("Placeholder Media")
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,69 +1,12 @@
-use std::collections::btree_map::Values;
+
+
+mod content;
+
+use content::media::Media;
+use content::catalog::Catalog;
 
 
 
-#[derive(Debug)]
-enum Media {
-    Book {  title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast ( u32 ),
-    Placeholder
-
-
-}
-
-
-impl Media {
-    fn description(&self) -> String {
-        match self {
-            Media::Book {title, author} => {
-                format!("Book - Title: {}, Author: {}", title, author)
-            },
-            Media::Movie {title, director} => {
-                format!("Movie - Title: {}, Director: {}", title, director)
-            },
-            Media::Audiobook {title} => {
-                format!("Audiobook - Title: {}", title)
-            },
-            Media::Podcast(episode) => {
-                format!("Podcast - Episode: {}", episode)
-            },
-            Media::Placeholder => {
-                String::from("Placeholder Media")
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>
-}
-
-enum ValusPresentOrNot<'a> {
-    ValuePresent (&'a Media),
-    ValueNotPresent
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![]}
-    }
-
-    fn add_item(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-
-        if(self.items.len() > index) {
-            Some(&self.items[index])
-        }else {
-            None
-        }
-    }
-}
 
 
 fn main() {
@@ -97,9 +40,15 @@ fn main() {
     catalog.add_item(placeholder);
     
 
-    let item = catalog.get_by_index(200);
+    match catalog.get_by_index(2) {
+        Some(item) => {
+            println!("Item {:#?}", item)
+        }
+        None => {
+            println!("Item not found")
+        }
+    }
 
-    println!("{:#?}", item.unwrap());
 
 
 }
